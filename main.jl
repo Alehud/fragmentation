07691dd1dddc11aff91b5a306f6a130697e77f9b
alehud <alehud1995@gmail.com>
2023-05-10 18:16:25 -0400

    Created main module of the library

--- main.jl
+++ main.jl
@@ -1,1 +1,7 @@
-print("Hello world")
+include("src/Fragmentation.jl")
+using .Fragmentation
+
+
+
+
+
---
+++ src/Fragmentation.jl
@@ -0,0 +1,12 @@
+module Fragmentation
+
+using IterTools
+using LinearAlgebra
+using Graphs
+using Random
+
+include("EntanglementEntropy.jl")
+include("HilbertSpace.jl")
+include("Utils.jl")
+
+end
