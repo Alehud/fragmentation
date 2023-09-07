a7777635e793cbcf8178f68118fb87f07d78b2a8
alehud <alehud1995@gmail.com>
2023-09-06 21:59:09 -0400

    Added plotting

--- bs_return_time.jl
+++ bs_return_time.jl
@@ -6,6 +6,10 @@ using Graphs
 using Karnak
 using Plots
 using Colors
+using LaTeXStrings
+using StatsBase
+using Polynomials
+using Printf
 
 # 0 = vacuum
 # 1 = a
@@ -67,7 +71,7 @@ end
 
 
 dof_dim = 5
-L = 10
+L = 12
 H_terms = Tuple[]
 for i in 1:(L-2)
     push!(H_terms, (1.0, [i, i+1, i+2], flippable, flip))
@@ -89,11 +93,41 @@ s_init = fill(Int8(0), L)
 println("s_init: $s_init")
 
 num_return_times = 10
-return_times = get_return_times(s_init, H, move!; num_returns=num_return_times)
+t = @elapsed begin
+    return_times = get_return_times(s_init, H, move!; num_returns=num_return_times)
+end
 
-serialize("data/fragile/bs_return_times_L$(L)_$(num_return_times ÷ 1000)k.dat", return_times)
+println("$t sec")
+returns_per_day = Int64(floor(24*60*60 / t))*num_return_times
+println(returns_per_day, " estimated returns in a day")
+flush(stdout)
 
-# histogram(return_times)
-# savefig("pics/bs_return_times_$(num_return_times)")
 
-# return_times = deserialize("data/fragile/bs_return_times_L$(L)_$(num_return_times ÷ 1000)k.dat")
+return_times = get_return_times(s_init, H, move!; num_returns=returns_per_day)
+serialize("data/fragile/bs_return_times_L$(L)_$(length(return_times) ÷ 1000)k.dat", return_times)
+
+
+
+L = 6
+return_times = deserialize("data/fragile/bs_return_times_L$(L).dat")
+h = StatsBase.fit(Histogram, return_times, nbins=50)
+edg = collect(h.edges[1])
+x_data = (edg[1:end-1] .+ edg[2:end])/2
+y_data = h.weights
+
+histogram(return_times, bins=edg, label="Numerics ($(length(return_times) ÷ 1000)k returns)", yscale=:log10, legend=:topright, color=:gray, minorgrid=true)
+
+linear_fit = Polynomials.fit(x_data, log.(replace(y_data, 0=>1)), 1)
+a = @sprintf "%.2e" exp(linear_fit.coeffs[1])
+b = @sprintf "%.2e" linear_fit.coeffs[2]
+
+x13_fit = Polynomials.fit(x_data.^(1/3), log.(replace(y_data, 0=>1)), 1)
+c = @sprintf "%.2f" x13_fit[1]
+d = @sprintf "%.2e" x13_fit[2]
+
+Plots.plot!(x_data, exp.(linear_fit.(x_data)), label=L"$ae^{bt}$", lw=3, color=:red, yscale=:log10, legend_font=10)
+# Plots.plot!(x_data, exp(linear_fit.coeffs[1]) * exp.(linear_fit.coeffs[2]*x_data.^(1/3)), label=L"$ae^{bt^{1/3}}$", lw=3, color=:green, yscale=:log10)
+title!("Return time probability distribution function \n Baumslag-Solitar group, L=$(L)", titlefontsize=10)
+xlabel!(L"t")
+ylabel!("# of returns")
+savefig("pics/bs_return_times_L$(L)")
