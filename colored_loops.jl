a6d50fa88c9e01c4b72b9d0685a690978e481bdf
alehud <alehud1995@gmail.com>
2023-09-06 21:59:54 -0400

    Optimized data types and some other changes

--- colored_loops.jl
+++ colored_loops.jl
@@ -1,7 +1,7 @@
 using IterTools
 using Combinatorics
 
-function N(n::Integer, label::Tuple{Vararg{Char}}, colors::Vector{Char}; pbc::Bool=false)
+function N(n::Integer, label::Union{Tuple{Vararg{Char}}, Vector{Char}}, colors::Vector{Char}; pbc::Bool=false)
     N_list = Int8[]
     cols_list = NTuple{n, Char}[]
     for cols in product(fill(colors, n)...)
@@ -166,38 +166,6 @@ colors = ['R', 'G', 'B']
 # println(N(2, label[L-1:end], colors))
 
 
-# L = 10
-# for label in product(fill(colors, L)...)
-#     P1 = N2(label[[1:5; 10]], colors)
-#     P2 = N2(label[5:10], colors)
-#     n1 = count(x -> x ≠ 0, P1)
-#     n2 = count(x -> x ≠ 0, P2)
-#     if n1 == 0 && n2 == 0
-#         PP = N2(label[[1:4; 6:9]], colors)
-#         n = count(x -> x ≠ 0, PP)
-#         if n ≠ 0
-#             noint = no_intersections(label)
-#             println(label, "  N2: ", PP, "  noint: ", noint)
-#         end
-#     end
-# end
-
-
-# # Check whether PP=0 implies P=0 (yes)
-# for label in product(fill(colors, L)...)
-#     P = N1(label, colors)
-#     n1 = count(x -> x ≠ 0, P)
-#     if n1 ≠ 0
-#         PP = N2(label, colors)
-#         n2 = count(x -> x ≠ 0, PP)
-#         if n2 == 0
-#             noint = no_intersections(label)
-#             println(label, ":  N1: ", P, "  N2: ", PP, "  noint: ", noint)
-#         end
-#     end
-# end
-
-
 # # Check whether PPP=0 implies PP=0 (if P=0) (3 colors: yes up to L=12)
 # for label in product(fill(colors, L)...)
 #     P = N1(label, colors)
@@ -236,35 +204,140 @@ colors = ['R', 'G', 'B']
 # end
 
 
-for L in [4, 6, 8, 10, 12, 14, 16, 18]
-    println("---------------------------------------------------------------------------------")
-    println("L: $L")
-    flag = false
-    # Check whether P=0, PP=0 and PPP=0 implies no intersections (3 colors: yes up to L=16)
-    for label in product(fill(colors, L)...)
-        P = N1(label, colors)
-        n1 = count(x -> x ≠ 0, P)
-        if n1 == 0
-            PP = N2(label, colors)
-            n2 = count(x -> x ≠ 0, PP)
-            if n2 == 0
-                PPP, _ = N(3, tuple(label...), colors)
-                n3 = count(x -> x ≠ 0, PPP)
-                if n3 == 0
-                    noint = no_intersections(label)
-                    if !noint
-                        flag = true
-                        println(label, ":  N1: ", P, "  N2: ", PP, "  N3: ", PPP, "  noint: ", noint)
+# for L in [4, 6, 8, 10, 12, 14, 16, 18]
+#     println("---------------------------------------------------------------------------------")
+#     println("L: $L")
+#     flag = false
+#     # Check whether P=0, PP=0 and PPP=0 implies no intersections (3 colors: yes up to L=16)
+#     for label in product(fill(colors, L)...)
+#         P = N1(label, colors)
+#         n1 = count(x -> x ≠ 0, P)
+#         if n1 == 0
+#             PP = N2(label, colors)
+#             n2 = count(x -> x ≠ 0, PP)
+#             if n2 == 0
+#                 PPP, _ = N(3, tuple(label...), colors)
+#                 n3 = count(x -> x ≠ 0, PPP)
+#                 if n3 == 0
+#                     noint = no_intersections(label)
+#                     if !noint
+#                         flag = true
+#                         println(label, ":  N1: ", P, "  N2: ", PP, "  N3: ", PPP, "  noint: ", noint)
+#                     end
+#                 end
+#             end
+#         end
+#     end
+#     if !flag
+#         println("For m=$(length(colors)) colors and L=$L, 1,2,3-index are enough for no intersection.")
+#     end
+#     flush(stdout)
+# end
+
+## Check whether indices on two loops sum up to indices on the concatenated loop
+idx_left = [1,2,3,4,5,6]
+idx_right = [7,8,9,10,6,5]
+idx_all = [1,2,3,4,7,8,9,10]
+L = maximum([idx_left; idx_right])
+L_left = length(idx_left)
+L_right = length(idx_right)
+
+# Check whether P1_left + P1_right = P1_all
+for label_left in product(fill(colors, L_left)...)
+    for label_rest in product(fill(colors, L - L_left)...)
+        label = [collect(label_left); collect(label_rest);]
+        P1_left = N1(label[idx_left], colors)
+        P1_right = N1(label[idx_right], colors)
+        P1_all = N1(label[idx_all], colors)
+        if P1_left .+ P1_right ≠ P1_all
+            println(label)
+            println("   N1 left: ", P1_left)
+            println("   N1 right: ", P1_right)
+            println("   N1 all: ", P1_all)
+        end
+    end
+end
+println("END")
+
+# Check whether P2_left = 0 and P2_right = 0 implies P2_all = 0
+for label_left in product(fill(colors, L_left)...)
+    P2_left = N2(label_left, colors)
+    n2_left = count(x -> x ≠ 0, P2_left)
+    if n2_left == 0
+        for label_rest in product(fill(colors, L - L_left)...)
+            label = [collect(label_left); collect(label_rest);]
+            P2_right = N2(label[idx_right], colors)
+            n2_right = count(x -> x ≠ 0, P2_right)
+            if n2_right == 0
+                P2_all = N2(label[idx_all], colors)
+                n2_all = count(x -> x ≠ 0, P2_all)
+                if n2_all ≠ 0
+                    println(label)
+                    println("   N2 left: ", P2_left)
+                    println("   N2 right: ", P2_right)
+                    println("   N2 all: ", P2_all)
+                end
+            end
+        end
+    end
+end
+println("END")
+
+# Check whether P1_left = 0 and P1_right = 0 implies P2_all = P2_left + P2_right
+for label_left in product(fill(colors, L_left)...)
+    P1_left = N1(label_left, colors)
+    n1_left = count(x -> x ≠ 0, P1_left)
+    if n1_left == 0
+        for label_rest in product(fill(colors, L - L_left)...)
+            label = [collect(label_left); collect(label_rest);]
+            P1_right = N1(label[idx_right], colors)
+            n1_right = count(x -> x ≠ 0, P1_right)
+            if n1_right == 0
+                P2_left = N2(label_left, colors)
+                P2_right = N2(label[idx_right], colors)
+                P2_all = N2(label[idx_all], colors)
+                if P2_left .+ P2_right ≠ P2_all
+                    println(label)
+                    println("   N2 left: ", P2_left)
+                    println("   N2 right: ", P2_right)
+                    println("   N2 all: ", P2_all)
+                end
+            end
+        end
+    end
+end
+println("END")
+
+# Check whether P2_left = 0 and P2_right = 0 (which immediately implies P1_left = 0, P1_right = 0) implies P3_all = P3_left + P3_right
+for label_left in product(fill(colors, L_left)...)
+    P1_left = N1(label_left, colors)
+    n1_left = count(x -> x ≠ 0, P1_left)
+    if n1_left == 0
+        P2_left = N2(label_left, colors)
+        n2_left = count(x -> x ≠ 0, P2_left)
+        if n2_left == 0
+            for label_rest in product(fill(colors, L - L_left)...)
+                label = [collect(label_left); collect(label_rest);]
+                P1_right = N2(label[idx_right], colors)
+                n1_right = count(x -> x ≠ 0, P1_right)
+                if n1_right == 0
+                    P2_right = N2(label[idx_right], colors)
+                    n2_right = count(x -> x ≠ 0, P2_right)
+                    if n2_right == 0
+                        P3_left = N(3, label_left, colors)
+                        P3_right = N(3, label[idx_right], colors)
+                        P3_all = N(3, label[idx_all], colors)
+                        if P3_left[1] .+ P3_right[1] ≠ P3_all[1]
+                            println(label)
+                            println("   N3 left: ", P3_left)
+                            println("   N3 right: ", P3_right)
+                            println("   N3 all: ", P3_all)
+                        end
                     end
                 end
             end
         end
     end
-    if !flag
-        println("For m=$(length(colors)) colors and L=$L, 1,2,3-index are enough for no intersection.")
-    end
-    flush(stdout)
 end
-
-
 println("END")
+
