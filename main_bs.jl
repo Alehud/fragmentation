e46046c19f166a6df4b2a7145396f7122bb39391
alehud <alehud1995@gmail.com>
2023-09-06 21:57:10 -0400

    Replaced explicit definition of dynamics with automatic Hamiltonian construction using interaction range

--- main_bs.jl
+++ main_bs.jl
@@ -1,10 +1,13 @@
 include("src/Fragmentation.jl")
 using .Fragmentation
+using IterTools
 using SparseArrays
 using Serialization
 using Graphs
+using Luxor
 using Karnak
 using Colors
+using Plots
 
 # 0 = vacuum
 # 1 = a
@@ -12,165 +15,131 @@ using Colors
 # 3 = a^(-1)
 # 4 = b^(-1)
 
-function annihilatable(s)
-    return s[1]*s[2] == 3 || s[1]*s[2] == 8
+e = Int64[1 0; 0 1]
+a = Int64[1 1; 0 1]
+ā = Int64[1 -1; 0 1]
+b = Int64[2 0; 0 1]
+b̄ = [1//2 0; 0 1]
+
+interaction_range = 3
+
+# Construct the generator set (a vector with matrices)
+generator_set = Matrix{Union{Int64, Rational{Int64}}}[e, a, b, ā, b̄]
+dof_dim = length(generator_set)
+
+
+# Construct a dictionary, where keys are computations (permutations) and values are vectors of states realizing this computation
+d = Dict{Matrix{Union{Int64, Rational{Int64}}}, Vector{Vector{Int8}}}()
+d_len = Dict{Matrix{Union{Int64, Rational{Int64}}}, Integer}()
+identity_states = Vector{Int8}[]
+for state in product(fill([0:(dof_dim-1);], interaction_range)...)
+    group_elem = copy(e)
+    for s in collect(state)
+        group_elem *= generator_set[s+1]
+    end
+    d[group_elem] = push!(get(d, group_elem, Vector{Int8}[]), collect(state))
+    d_len[group_elem] = get(d_len, group_elem, 0) + 1
+    if group_elem == e
+        push!(identity_states, collect(state))
+    end
 end
+# findfirst(states -> [0,0,0] in states, d)     # how to find a permutation realized by a specific state
+# filter!(p -> length(p.second) > 1, d)   # Remove states that are not flippable
+# flippable_states = collect(values(d))    # leave only values (states)
 
-function annihilate(s)
-    return [0, 0]
-end
-
-function creatable(s)
-    return s[1] == 0 && s[2] == 0
-end
-
-function create_a1(s)
-    return [1, 3]
-end
-
-function create_a2(s)
-    return [3, 1]
-end
-
-function create_b1(s)
-    return [2, 4]
-end
-
-function create_b2(s)
-    return [4, 2]
-end
-
-function flippable(s)
-    return s[1] == 2 && s[2] == 1 && s[3] == 0
-end
-
-function flip(s)
-    return [1,1,2]
-end
-
-function flippable_back(s)
-    return s[1] == 1 && s[2] == 1 && s[3] == 2
-end
-
-function flip_back(s)
-    return [2,1,0]
-end
-
-function hoppable(s)
-    return s[1] * s[2] == 0 && s[1] + s[2] ≠ 0
-end
-
-function hop(s)
-    return [s[2], s[1]]
-end
+# serialize("data/fragile/d_i$(interaction_range).dat", d)
+# serialize("data/fragile/d_len_i$(interaction_range).dat", d_len)
+# serialize("data/fragile/identity_states_i$(interaction_range).dat", identity_states)
 
 
-dof_dim = 5
-L = 6
+# Construct Hamiltonian
+L = 7
 H_terms = Tuple[]
-for i in 1:(L-2)
-    push!(H_terms, (1.0, [i, i+1, i+2], flippable, flip))
-    push!(H_terms, (1.0, [i, i+1, i+2], flippable_back, flip_back))
+for site in 1:(L-(interaction_range-1))
+    for (group_element, states) in d
+        len = length(states)
+        for i in 1:len, j in 1:len
+            if i ≠ j
+                push!(H_terms, (1.0, [site:site+(interaction_range-1);], x -> x == states[i], x -> states[j]))
+            end
+        end
+    end
 end
-for i in 1:(L-1)
-    push!(H_terms, (1.0, [i, i+1], annihilatable, annihilate))
-    push!(H_terms, (1.0, [i, i+1], creatable, create_a1))
-    push!(H_terms, (1.0, [i, i+1], creatable, create_a2))
-    push!(H_terms, (1.0, [i, i+1], creatable, create_b1))
-    push!(H_terms, (1.0, [i, i+1], creatable, create_b2))
-    push!(H_terms, (1.0, [i, i+1], hoppable, hop))
+println(length(H_terms)÷2, " terms in the Hamiltonian")
+H = Hamiltonian(dof_dim, H_terms, check_hermitian=true);
+
+
+d = deserialize("data/fragile/d_i$(L).dat")
+gg = SimpleGraph{Int64}[]
+legend = ["0" => "_", "1" => "a", "2" => "b", "3" => "ā", "4" => "b̄"]
+for (perm, states_symm_sector) in d
+    # Connectivity within symmetry sector
+    states_symm_sector_list, hams = explore_full_space(H, states_symm_sector; construct_ham=true)
+    g = SimpleGraph()
+    i = 0
+    for (states, ham) in zip(states_symm_sector_list, hams)
+        rows, cols, mels = findnz(ham);
+        rows .+= i
+        cols .+= i
+        edges = Edge.(collect(zip(rows, cols)))
+        g_temp = SimpleGraph(edges)
+        if isempty(edges)
+            add_vertices!(g, length(states))
+        else
+            g = union(g, g_temp)
+        end
+        i += length(states)
+
+        # Identify states up to identity generators
+        state_labels = vertex_labels(states, legend)
+        remove_vacuum!(state_labels, g)
+    end
+    push!(gg, g)
 end
 
-H = Hamiltonian(dof_dim, H_terms, check_hermitian=true)
-
-# for H_terms in H.H_terms
-#     println(H_terms)
-# end
-
-s_init = fill(Int8(0), L)
-println("s_init: $s_init")
-states, ham = explore_connected_states(s_init, H, construct_ham=true)
-println("$(length(states)) states found", )
-# println("states:")
-# for state in states
-#     println(state)
-# end
-# println("ham: ", ham)
-display(ham)
-rows, cols, mels = findnz(ham)
-# println("rows: $rows")
-# println("cols: $cols")
-# println("mels: $mels")
-
-# states_all, hams = explore_full_space(H, 8, construct_ham=true)
-# for (i, states) in enumerate(states_all)
-#     println(i, " ", states)
-# end
-
-# rows = []
-# cols = []
-# mels = []
-
-# function f()
-#     i = 0
-#     for (ham, states) in zip(hams, states_all)
-#         rows_temp, cols_temp, mels_temp = findnz(ham)
-#         append!(rows, rows_temp .+ i)
-#         append!(cols, cols_temp .+ i)
-#         append!(mels, mels_temp)
-#         i += length(states)
-#     end
-#     print(i)
-#     println(rows)
-#     println(cols)
-# end
-
-# f()
 
 
-edges = Edge.(collect(zip(rows, cols)))
-g = SimpleGraph(edges)
-legend = ["0" => "_", "1" => "a", "2" => "b", "3" => "A", "4" => "B"]
-state_labels = vertex_labels(states, legend)
-remove_vacuum!(state_labels, g)
-println("$(length(state_labels)) states left after removing vacuum.")
+window_height = 10000
+window_width = 10000
 
-
-y = [length(a_star(g, 1, v)) for v in vertices(g)]
-window_height = 4000
-window_width = 4000
-pts = vcat([between.(O + (-window_width/2, window_height/2 - d*window_height/maximum(y)), O + (window_width/2, window_height/2 - d*window_height/maximum(y)), range(0, 1, length=count(i -> i == d, y)+2))[2:end-1] 
-            for d in 0:maximum(y)]...)[invperm(sortperm(y))]
+# y = [length(a_star(g, 1, v)) for v in vertices(g)]
+# pts = vcat([between.(O + (-window_width/2, window_height/2 - d*window_height/maximum(y)), O + (window_width/2, window_height/2 - d*window_height/maximum(y)), range(0, 1, length=count(i -> i == d, y)+2))[2:end-1] 
+#             for d in 0:maximum(y)]...)[invperm(sortperm(y))]
 # Stress(initialpos = [(pt.x, pt.y) for pt in pts], iterations = 100)
 
 
-@pdf begin
-    background("grey10")
-    sethue("orange")
-    drawgraph(g, layout = spring,
-    vertexshapes = (vtx) -> box(O, 10(L+1), 20, :fill),
-    vertexlabels = state_labels,
-    vertexlabelfontsizes = 20,
-    vertexshapesizes = 12,
-    vertexlabeltextcolors = colorant"black",
-    vertexlabeloffsetdistances = 0,
-    edgelines = (k, s, d, f, t) ->
-        arrow(f, t, arrow_func(s, d, y),
-            linewidth = 3,
-            arrowheadlength = 0),
-    edgestrokeweights = 4,
-    edgecurvature = 30)
-end window_height+100 window_width+100 "pics/bs_test"
-
+colors = ["paleturquoise", "chartreuse", "thistle1", "pink",
+"gold", "wheat", "olivedrab1", "palegreen", "turquoise1",
+"lightgreen", "plum1", "plum", "violet", "hotpink"]
 
 @pdf begin
     background("grey10")
     sethue("orange")
-    drawgraph(g, layout = pts,
-    vertexshapesizes = 4,
-    edgelines = (k, s, d, f, t) ->
-        arrow(f, t, arrow_func(s, d, y),
-            linewidth = 1,
-            arrowheadlength = 0),
-    edgestrokeweights = 2)
-end window_height+100 window_width+100 "pics/bs_nolabel_test"
+    ng = length(gg)
+    N = convert(Int, ceil(sqrt(ng)))
+    tiles = Tiler(window_height, window_width, N, N)
+    setline(0.5)
+    for ((pos, n), (perm, _)) in zip(tiles, d)
+        @layer begin
+            n > ng && break
+            Karnak.translate(pos)
+            sethue("gray")
+            bbox = BoundingBox(box(O, tiles.tilewidth, tiles.tileheight, :stroke))
+            if nv(gg[n]) == 1
+                layout = [Point(0.0,0.0)]
+            else
+                layout = spring
+            end
+            sethue(colors[mod1(n, end)])
+            drawgraph(gg[n], layout = layout,
+            boundingbox = bbox,
+            vertexshapesizes = 4,
+            vertexfillcolors = colorant"red",
+            edgestrokeweights = 1)
+            sethue("cyan")
+            fontsize(14)
+            Karnak.text(string(Float64.(perm[1,:]))[2:end-1], halign = :center, boxbottomcenter(bbox) - (0, 20))
+            Karnak.text(string(Float64.(perm[2,:]))[2:end-1], halign = :center, boxbottomcenter(bbox) - (0, 5))
+        end
+    end
+end window_height+100 window_width+100 "pics/fragile/connectivity_full_merged_L$(L)_i$(interaction_range)"
